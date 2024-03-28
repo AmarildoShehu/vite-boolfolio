697bfa7eebfe87a672fcfee2a6a96576249b1da2
Amarildo <you@example.com>
2024-03-28 19:19:09 +0100

    added Appheader

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+  name: 'Boolpress',
+  components: { AppHeader }
+}
+</script>
+
+<template>
+
+  <AppHeader />
+  <main class="container">
+    <h1>Boolpress</h1>
+
+  </main>
+
+
+
+</template>
+
+<style></style>
