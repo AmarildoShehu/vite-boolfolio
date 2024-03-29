683ac76dce9b6cd17521fce2b4f97c2a9cdc6cea
Amarildo <you@example.com>
2024-03-29 11:06:33 +0100

    millestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
-
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
 
 export default {
   name: 'Boolpress',
-  components: { AppHeader }
-}
+  components: { AppHeader },
+  data: () => ({ projects: [] }),
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data);
+
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+};
 </script>
 
 <template>
@@ -14,6 +28,13 @@ export default {
   <main class="container">
     <h1>Boolpress</h1>
 
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h3 v-text="project.title">
+
+        </h3>
+      </li>
+    </ul>
   </main>
 
 
